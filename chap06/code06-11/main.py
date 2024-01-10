2172a538ac4e64eaa6e1580b9176554dcebcea5b
Shinpei KUDO <kudo@miyabilink.jp>
2024-01-10 15:53:38 +0900

    Zendesk#1323　誤植対応

--- chap06/code06-11/main.py
+++ chap06/code06-11/main.py
@@ -4,6 +4,6 @@ names.append('松田')
 print(f'list（変更後）: {id(names)}')
 
 name = '松田'   # 文字列の場合
-print(f'str（変更前）: {id(names)}')
+print(f'str（変更前）: {id(name)}')
 name = 'スーパー' + name
-print(f'str（変更後）: {id(names)}')
+print(f'str（変更後）: {id(name)}')
